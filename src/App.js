44a00047e3abf2525ee283ea86c797c72e95ca32
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-05 16:51:49 -0600

    passing information from components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import TaskCreator from "./components/TaskCreator";
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
 
-export default App
+  const [tasksItems, setTaskItems] = useState([
+    {name: "mi primer tarea" , done: false}
+  ])
+
+  function createTask(taskValue){
+    setTaskItems([...tasksItems, {name:taskValue, done:false}])
+
+
+  }
+
+  return (
+    <div>
+      <TaskCreator createTask={createTask} />
+      <table>
+        <thead>
+        <tr>
+          <td>
+            Task
+          </td>
+        </tr>
+        </thead>
+        <tbody>
+          {
+            tasksItems.map((task, index )=>(
+              <tr key={index}>
+                <td>
+                {task.name}
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
+    </div>
+  );
+};
+
+export default App;
