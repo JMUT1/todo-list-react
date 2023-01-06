f35d6b31a0bdc6fbfc216f44cd0747aa3447bfb0
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-06 08:58:24 -0600

    flex in rows

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,20 +1,16 @@
-
-const TaskRow = ({task, toogleTask}) => {
+const TaskRow = ({ task, toogleTask }) => {
   return (
-    
     <tr key={task.name}>
-    <td>
-    {task.name}
-    <input type="checkbox"
-    checked={task.done}
-    onChange={()=> toogleTask(task)}
-    
-    />
-    </td>
-  </tr>
+      <td className="d-flex justify-content-between">
+        {task.name}
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={() => toogleTask(task)}
+        />
+      </td>
+    </tr>
+  );
+};
 
-
-    )
-}
-
-export default TaskRow
+export default TaskRow;
